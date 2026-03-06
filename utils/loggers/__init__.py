f307a20d8e6c243c95b4bcc61adc017b480f664e
UltralyticsAssistant <web@ultralytics.com>
2026-03-06 04:11:12 +0000

    Auto-format by https://ultralytics.com/actions

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -87,10 +87,21 @@ class Loggers:
         self.logger = logger  # for printing results to console
         self.include = include
         self.keys = [
-            'train/box_loss', 'train/obj_loss', 'train/cls_loss', 'train/distill',  # [新增] train/distill
-            'metrics/precision', 'metrics/recall', 'metrics/mAP_0.5', 'metrics/mAP_0.5:0.95',  # metrics
-            'val/box_loss', 'val/obj_loss', 'val/cls_loss', 'val/distill',          # [新增] val/distill
-            'x/lr0', 'x/lr1', 'x/lr2'
+            "train/box_loss",
+            "train/obj_loss",
+            "train/cls_loss",
+            "train/distill",  # [新增] train/distill
+            "metrics/precision",
+            "metrics/recall",
+            "metrics/mAP_0.5",
+            "metrics/mAP_0.5:0.95",  # metrics
+            "val/box_loss",
+            "val/obj_loss",
+            "val/cls_loss",
+            "val/distill",  # [新增] val/distill
+            "x/lr0",
+            "x/lr1",
+            "x/lr2",
         ]  # params
         self.best_keys = ["best/epoch", "best/precision", "best/recall", "best/mAP_0.5", "best/mAP_0.5:0.95"]
         for k in LOGGERS:
